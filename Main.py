699f9e84d4b7dbc68436ab3aee6e618cf9938c01
winson-cell <winsheng2000@gmail.com>
2021-01-01 12:14:02 +0800

    read multiuple link

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
 from Api import Api
 from Read_data import File
-from Sound import playing_sound_repeatedly
 
 if __name__ == '__main__':
     file = File()
     api_arr = []
     for i in range(file.size):
         api_arr.append(Api(int(file.price[i]), file.link[i]))
-    api_arr[0].retrive_and_check()
+    while True:
+        for i in range(file.size):
+            api_arr[i].retrive_and_check()
