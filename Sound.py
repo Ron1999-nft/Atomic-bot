67f02ead30fb9fd2d327da37e2eff20289836e05
winson-cell <winsheng2000@gmail.com>
2021-01-01 12:26:55 +0800

    adding the mulktiple file functionality

--- Sound.py
+++ Sound.py
@@ -2,10 +2,10 @@ from playsound import playsound
 import keyboard
 
 def playing_sound_repeatedly(): # -> loop
-    print('Press C to exist the sound')
+    print('Press C to exit the sound')
     while True:
         # python file\PLay beep sound\ is edited from path file
         playsound('Computer-beep-beeping-1-www.FesliyanStudios.com.mp3') # play the sound
         if keyboard.is_pressed('c'):
             print('C has been pressed, programme will be existed')
-            break
+            exit()
