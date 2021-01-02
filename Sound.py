05b2c73b2b5ada9d38b292d0b253d121908064f8
winson-cell <winsheng2000@gmail.com>
2021-01-02 11:47:34 +0800

    changes to sound

--- Sound.py
+++ Sound.py
@@ -1,7 +1,4 @@
 from playsound import playsound
-import keyboard, time
 
-def playing_sound_repeatedly(): # -> loop
-    t_end = time.time() + 60
-    while time.time() < t_end:
-        playsound('Computer-beep-beeping-1-www.FesliyanStudios.com.mp3')  # play the sound
+def playing_sound_repeatedly():
+    playsound('Computer-beep-beeping-1-www.FesliyanStudios.com.mp3')  # play the sound
