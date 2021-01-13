2139ea3ba086ce19bc74a594f90c3cbe2d47c40e
winson-cell <winsheng2000@gmail.com>
2021-01-13 12:03:42 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,28 @@
 from Api import Api
 from Read_data import File
 
+#---------PRINTING COLOUR------------------#
+def prYellow(text):
+    print('\033[33m', text, '\033[0m', sep='')
+#---------PRINTING COLOUR------------------#
+
 if __name__ == '__main__':
     """
     Main which read file
     """
-    file = File()
-    api_arr = []
-    for i in range(file.size):
-        api_arr.append(Api(float(file.price[i]), file.link[i]))
     while True:
-        for i in range(file.size):
-            try:
-                api_arr[i].retrive_and_check()
-            except:
-                continue
+        filename = "Atomic.txt"
+        filehandle = open(filename, 'r')
+        while True:
+            line = filehandle.readline()
+            if not line:
+                filehandle.close()
+                break
+            while line[0] == "*":
+                line = line.strip("\n")
+                prYellow(line)
+                line = filehandle.readline()
+            line = line.strip("\n")
+            line = line.split(" ")
+            api = Api(float(line[0]),line[1])
+            api.retrive_and_check()
